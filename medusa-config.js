646333b001894bb54c7bbfa9c637da947b940b85
worldbestpro <ilkhy277@gmail.com>
2022-12-26 22:30:53 +0000

    s3 file plugin

--- medusa-config.js
+++ medusa-config.js
@@ -83,6 +83,16 @@ const plugins = [
       },
     },
   },
+  {
+    resolve: `medusa-file-s3`,
+    options: {
+      s3_url: process.env.S3_URL,
+      bucket: process.env.S3_BUCKET,
+      region: process.env.S3_REGION,
+      access_key_id: process.env.S3_ACCESS_KEY_ID,
+      secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+    },
+  },
 ];
 
 module.exports = {
